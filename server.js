c3db335bdf5e3071fd1569077399d35cdf79470e
Vagesh Bangarigalla <vagesh242@gmail.com>
2025-08-14 07:37:30 +0530

    port changed 3000 to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 );
